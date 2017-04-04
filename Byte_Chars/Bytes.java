dfe161cfb0306567f568f0025fee46e91405f95c
Saransh Agarwal <saransh808@gmail.com>
2017-04-05 00:53:01 +0530

    #bytes

---
+++ Byte_Chars/Bytes.java
@@ -0,0 +1,31 @@
+package lab1;
+
+public class Bytes {
+	//Program to generate random Strings
+	public static void main(String arsg[]){
+	//	byte b = 97;
+	//	System.out.println((char)b);
+	//	char ch = 'a';
+	//	System.out.println((byte)ch);
+		
+		
+		//using Math.random() method;
+		for(int i=0;i<10;i++){
+			byte e=0; 
+			do{
+				e=(byte)(Math.random()*100);
+			}while((e<65 || e>90) ||
+					((char)e !='S') &&
+					((char)e !='A') &&
+					((char)e !='R') &&
+					((char)e !='A') &&
+					((char)e !='N') &&
+					((char)e !='S') &&
+					((char)e !='H') 
+					);
+				
+				System.out.print((char)e);	
+		}
+	}
+
+}
